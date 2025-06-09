2656043f9407b4ac74e08dd5c53668bf89b48b8f
LUIS EDUARDO BARROCAS ALVES BITU <helderbitu13@gmail.com>
2025-06-08 23:14:26 -0300

    habilitando o discovery client

--- src/main/java/br/unifor/fazeragendamento/FazeragendamentoApplication.java
+++ src/main/java/br/unifor/fazeragendamento/FazeragendamentoApplication.java
@@ -2,8 +2,10 @@ package br.unifor.fazeragendamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FazeragendamentoApplication {
 
 	public static void main(String[] args) {
