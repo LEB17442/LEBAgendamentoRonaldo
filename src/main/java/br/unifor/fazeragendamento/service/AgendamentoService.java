7d2c527f12ec84600fd5ca54e902d3546ab82d52
LUIS EDUARDO BARROCAS ALVES BITU <helderbitu13@gmail.com>
2025-06-09 00:32:50 -0300

    detalhe

--- src/main/java/br/unifor/fazeragendamento/service/AgendamentoService.java
+++ src/main/java/br/unifor/fazeragendamento/service/AgendamentoService.java
@@ -7,7 +7,7 @@ import br.unifor.fazeragendamento.model.Agendamento;
 import br.unifor.fazeragendamento.repository.AgendamentoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
